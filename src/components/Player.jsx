0cb29cfdbd401d9ea965cb55ce4efdfcf94ef388
phuocleoceo <ht10082001@gmail.com>
2021-07-07 18:13:14 +0700

    React H5 Audio Player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,12 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { Nav, Navbar, Image } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 export default function Player() {
 	const play = useSelector(state => state.play);
-	const audioRef = useRef();
-	useEffect(() => {
-		const dom = audioRef.current;
-		dom.src = play.music;
-	}, [play])
-	//State for src cannot make component re-render != img src
+
 	return (
 		<Navbar bg="dark" expand="lg" fixed="bottom">
 			<Nav>
@@ -24,10 +21,12 @@ export default function Player() {
 					<p className="pCreator">{play.creator}</p>
 				</Nav.Item>
 
-				<audio controls autoPlay ref={audioRef}>
-					<source src=""
-						type="audio/mp3" />
-				</audio>
+				<div className="player-bar">
+					<AudioPlayer
+						autoPlay
+						src={play.music}
+					/>
+				</div>
 			</Nav >
 		</Navbar >
 	)
