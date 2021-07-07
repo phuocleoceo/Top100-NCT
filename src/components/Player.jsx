3b563b141d867f1f839c1b7eb89c9ac428040863
phuocleoceo <ht10082001@gmail.com>
2021-07-07 20:50:51 +0700

    Simple Player Bar

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Nav, Navbar, Image } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
@@ -8,26 +7,11 @@ export default function Player() {
 	const play = useSelector(state => state.play);
 
 	return (
-		<Navbar bg="dark" expand="lg" fixed="bottom">
-			<Nav>
-				<Image
-					width="100 % "
-					height="auto"
-					src={play.avatar}
-					className="img-avatar-play"
-				/>
-				<Nav.Item className="infor-play">
-					<p className="pTitle">{play.title}</p>
-					<p className="pCreator">{play.creator}</p>
-				</Nav.Item>
-
-				<div className="player-bar">
-					<AudioPlayer
-						autoPlay
-						src={play.music}
-					/>
-				</div>
-			</Nav >
-		</Navbar >
+		<AudioPlayer
+			className="player-bar"
+			style={{ backgroundColor: "black" }}
+			autoPlay
+			src={play.music}
+		/>
 	)
 }
