5d90d092d609007483fb82bd01df3193f3453970
phuocleoceo <ht10082001@gmail.com>
2021-07-09 18:50:03 +0700

    Remove Overlay&PT on Mobile

--- src/components/SongListCard.jsx
+++ src/components/SongListCard.jsx
@@ -24,8 +24,7 @@ export default function SongListCard(props) {
 									<Card.Title>{s.title}</Card.Title>
 									<Card.Text>{s.creator}</Card.Text>
 									<div className="overlay"></div>
-									<Card.Img src="/playbutton.png" className="playbutton"
-										onClick={() => dispatch(setCurrentPlay(s))} />
+									<Card.Img src="/playbutton.png" className="playbutton" />
 								</Card.Body>
 							</Card>
 						</Col>)
