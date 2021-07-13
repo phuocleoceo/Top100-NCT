0f573cd667a038e89c84d979b57147633ab9140a
phuocleoceo <ht10082001@gmail.com>
2021-07-13 22:45:50 +0700

    Fix change Title

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchSongList } from './redux/slices/songSlice';
 import SongListCard from './components/SongListCard';
 import Player from './components/Player';
 import Header from './components/Header';
 
 function App() {
+	const play = useSelector(state => state.play);
 	const dispatch = useDispatch();
 	useEffect(() => {
 		dispatch(fetchSongList());
 	}, [dispatch]);
+
+	useEffect(() => {
+		if (play.music) {
+			document.title = play.title + " - " + play.creator;
+		}
+	}, [play]);
 
 	return (
 		<>
